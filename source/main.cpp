d5b2c3ede87c5b0082a57c11deb533963b74d756
p0mpurin <purin@dev.com>
2026-06-29 23:27:10 +0200

    Add RuneFetch folder status probe

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,56 @@
 #include <3ds.h>
+#include <cstdio>
 
 namespace {
 alignas(8) u8 g_static_heap[0x10000];
+
+void write_file(const char *path, const char *data, u32 size)
+{
+	Handle file = 0;
+	Result open_res = FSUSER_OpenFileDirectly(&file, ARCHIVE_SDMC,
+		fsMakePath(PATH_EMPTY, nullptr),
+		fsMakePath(PATH_ASCII, path),
+		FS_OPEN_CREATE | FS_OPEN_WRITE, 0);
+	if(R_FAILED(open_res))
+		return;
+
+	u32 written = 0;
+	FSFILE_Write(file, &written, 0, data, size, FS_WRITE_FLUSH);
+	FSFILE_Close(file);
+}
+
+void write_boot_marker(const char *stage)
+{
+	char data[96];
+	int len = snprintf(data, sizeof(data), "stage=%s\nresult=00000000\n", stage);
+	if(len > 0)
+		write_file("/runefetch_boot.txt", data, static_cast<u32>(len));
+}
+
+Result create_dir(FS_Archive archive, const char *path)
+{
+	return FSUSER_CreateDirectory(archive, fsMakePath(PATH_ASCII, path), 0);
+}
+
+Result ensure_dirs()
+{
+	FS_Archive archive;
+	Result res = FSUSER_OpenArchive(&archive, ARCHIVE_SDMC, fsMakePath(PATH_EMPTY, nullptr));
+	if(R_FAILED(res))
+		return res;
+
+	create_dir(archive, "/3ds");
+	create_dir(archive, "/3ds/Rune3DS");
+	create_dir(archive, "/3ds/Rune3DS/runefetch");
+	create_dir(archive, "/3ds/Rune3DS/runefetch/jobs");
+	create_dir(archive, "/3ds/Rune3DS/runefetch/done");
+	create_dir(archive, "/3ds/Rune3DS/runefetch/failed");
+	create_dir(archive, "/3ds/Rune3DS/runefetch/state");
+	create_dir(archive, "/3ds/Rune3DS/cache");
+
+	FSUSER_CloseArchive(archive);
+	return 0;
+}
 }
 
 extern "C" {
@@ -37,17 +86,18 @@ int main()
 	Result res = fsInit();
 	if(R_SUCCEEDED(res))
 	{
-		static const char data[] = "stage=fsInit\nresult=00000000\n";
-		Handle file = 0;
-		Result open_res = FSUSER_OpenFileDirectly(&file, ARCHIVE_SDMC,
-			fsMakePath(PATH_EMPTY, nullptr),
-			fsMakePath(PATH_ASCII, "/runefetch_boot.txt"),
-			FS_OPEN_CREATE | FS_OPEN_WRITE, 0);
-		if(R_SUCCEEDED(open_res))
+		write_boot_marker("fsInit");
+
+		res = ensure_dirs();
+		if(R_SUCCEEDED(res))
 		{
-			u32 written = 0;
-			FSFILE_Write(file, &written, 0, data, sizeof(data) - 1, FS_WRITE_FLUSH);
-			FSFILE_Close(file);
+			write_boot_marker("dirs");
+			static const char status[] =
+				"state=probe\n"
+				"result=00000000\n"
+				"message=RuneFetch folders ready\n";
+			write_file("/3ds/Rune3DS/runefetch/state/status.txt",
+				status, sizeof(status) - 1);
 		}
 	}
 
